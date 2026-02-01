51c4197c70c1e9455b97ee82137c9cbacc7f2cec
Caterina E <caterinaesposito702@gmail.com>
2026-01-31 22:05:20 -0500

    Updates

---
+++ precache-manifest.59a8b0e2abff3cc734c47324d9db1678.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05d7de9f2695ff086da53e034ed5581",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "c61755427516952c77f6",
+    "url": "/portfolio-website/static/css/2.ff198c92.chunk.css"
+  },
+  {
+    "revision": "bf838b91a7f0b3444019",
+    "url": "/portfolio-website/static/css/main.3835b1c4.chunk.css"
+  },
+  {
+    "revision": "c61755427516952c77f6",
+    "url": "/portfolio-website/static/js/2.12f04ef0.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/portfolio-website/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "bf838b91a7f0b3444019",
+    "url": "/portfolio-website/static/js/main.f0081573.chunk.js"
+  },
+  {
+    "revision": "9e5c1b956049de4588cf",
+    "url": "/portfolio-website/static/js/runtime~main.f1845076.js"
+  },
+  {
+    "revision": "e14fa91205613f0b7e5f66147cefad53",
+    "url": "/portfolio-website/static/media/CE logo.e14fa912.png"
+  },
+  {
+    "revision": "2cbe556c4fa32e402427dfd8c317ad36",
+    "url": "/portfolio-website/static/media/Caterina_Esposito_Pic.2cbe556c.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio-website/static/media/slick.f97e3bbf.svg"
+  }
+]);
